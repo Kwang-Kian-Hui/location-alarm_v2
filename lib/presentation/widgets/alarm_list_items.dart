6ee691e520773c8e8e5cfcfd80918f886422af87
Kwang Kian Hui <2k1h297@gmail.com>
2021-08-22 23:44:29 +0800

    implement basic ui structure for AlarmListItem

--- lib/presentation/widgets/alarm_list_items.dart
+++ lib/presentation/widgets/alarm_list_items.dart
@@ -8,8 +8,17 @@ class AlarmListItem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final alarmData = ref.watch(currentAlarmItem);
-    return Container(
-      
+    return ListTile(
+      leading: Text(alarmData.alarmName),
+      title: Text('${alarmData.alarmRadius.toStringAsFixed(2)} m'),
+      subtitle: Text('${alarmData.destLat} + ${alarmData.destLng}'),
+      trailing: ListTile(
+        leading: Switch(
+          value: alarmData.alarmStatus,
+          onChanged: (newAlarmStatus){}, //TODO: do change alarmStatus
+        ),
+        trailing: Container(), //TODO: do menu for edit and delete
+      ),
     );
   }
 }
